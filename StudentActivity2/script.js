20a77a160aeedf5b3938466709f864e07a958792
“IvanARodriguez” <ivancalvo91@gmail.com>
2022-03-30 19:38:58 -0500

    adding content to practices 2

---
+++ StudentActivity2/script.js
@@ -0,0 +1,8 @@
+const getImgs = document.querySelectorAll('img');
+getImgs[0].setAttribute('src', 'https://images.unsplash.com/photo-1648569732645-ac8616452917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1600&q=60');
+getImgs[0].setAttribute('width', '500');
+
+const firstTableRow = document.getElementById("firstTableRow");
+firstTableRow.style.backgroundColor = "lightgrey"
+
+const columns = document.getElementsByClass("col");
